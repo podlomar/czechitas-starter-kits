6f74f80bf4df45de3e8e4fa80c9fccc35f3e00b6
Martin Podloucký <martin.podloucky@newtontech.cz>
2022-08-26 14:40:10 +0200

    New starter kit: React + TypeScript + ESLint

---
+++ kits/react-ts-eslint/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  ignorePatterns: ['.eslintrc.js'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-console': 'off',
+    'arrow-body-style': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': ['error', {
+      namedComponents: 'arrow-function',
+      unnamedComponents: 'arrow-function',
+    }],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+  },
+  // overrides: [{
+  //   files: ['*.ts', '*.tsx'],
+  //   parserOptions: {
+  //     project: './tsconfig.json',
+  //   },
+  //   rules: {
+  //     '@typescript-eslint/explicit-function-return-type': 'error',
+  //   },
+  // }],
+};
---
+++ kits/react-ts-eslint/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  devtool: 'eval-source-map',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'img/[name]-[hash:6].[ext]',
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/,
+        type: 'asset/resource'
+      }
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'public', to: '', noErrorOnMissing: true },
+      ],
+    }),
+  ],
+};
